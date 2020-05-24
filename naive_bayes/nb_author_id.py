0d120fe017b5fe8855a9d18beef3e0425ff3020f
Martin Arroyo <marroyo7775@gmail.com>
2020-05-24 00:20:44 -0400

    initial commit - miniproject 1 - Naive Bayes

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -1,10 +1,10 @@
-#!/usr/bin/python
+    #!/usr/bin/python
 
 """ 
     This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
 
     Use a Naive Bayes Classifier to identify emails by their authors
-    
+
     authors and labels:
     Sara has label 0
     Chris has label 1
@@ -12,7 +12,8 @@
     
 import sys
 from time import time
-sys.path.append("../tools/")
+sys.path.append('/home/marroyo/Programming/MachineLearning/IntrotoML/ud120-projects/tools/')
+print("The path is {} ".format(sys.path))
 from email_preprocess import preprocess
 
 
@@ -26,7 +27,17 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
-
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
+t0 = time() # Time to train
+clf.fit(features_train,labels_train)
+print "training time:", round(time()-t0, 3), "s"
+t1 = time() # Time to predict
+predict = clf.predict(features_test)
+print "prediction time:", round(time()-t1, 3), "s"
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(predict, labels_test,normalize=True)
+print("The accuracy of this training model is {}".format(accuracy))
 
 #########################################################
 
